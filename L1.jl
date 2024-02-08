23f4e87cc11205f4bcdf246d12568b55573bb8bc
AritraBrahmachari <55792362+AritraBrahmachari@users.noreply.github.com>
2024-02-08 21:14:15 +0530

    Update L1.jl

--- L1.jl
+++ L1.jl
@@ -1,15 +1,14 @@
 # OPTIONS FOR WHAT ROUTINES SHOULD BE RAN
 DO_CSA = false #perform Cartan Sub-Algebra (CSA) decomposition of Hamiltonian
-DO_DF = true #perform Double-Factorization (DF) decomposition of Hamiltonian
-DO_ΔE = true #obtain lower bound ΔE/2 of Hamiltonian, only for small systems!
-FOCK_BOUND = true #obtain lower bound for ΔE/2 using Fock matrix, can be done when ΔE is not accessible!
+DO_DF = false #perform Double-Factorization (DF) decomposition of Hamiltonian
+DO_ΔE = false #obtain lower bound ΔE/2 of Hamiltonian, only for small systems!
 DO_AC = true #do anticommuting grouping technique
 DO_OO = false #do orbital optimization routine
-DO_SQRT = false #obtain square-root factorization 1-norms
-DO_MHC = false #do Majorana Hyper-Contraction routine (SVD-based MTD-1ˆ4)
+DO_SQRT = true #obtain square-root factorization 1-norms
+DO_MHC = true #do Majorana Hyper-Contraction routine (SVD-based MTD-1ˆ4)
 DO_MTD_CP4 = false #MTD-1ˆ4
 DO_THC = false #tensor hypercontraction
-SYM_SHIFT = true #do symmetry-shift optimization routine (i.e. partial shift)
+SYM_SHIFT = false #do symmetry-shift optimization routine (i.e. partial shift)
 INT = false #do interaction picture optimization routines
 verbose = true #verbose for sub-routines
 COUNT = true #whether total number of unitaries should be counted
@@ -17,15 +16,17 @@ BLISS = true #whether block-invariant symmetry shift routine is done
 DO_TROTTER = false #whether Trotter α is calculated, requires parallel routines
 DO_FC = false #whether fully-commuting routine is done
 TABLE_PRINT = true #whether final 1-norms are printed for copy-pasting in LaTeX table
-SYM_SUBSPACE = false #whether spectral range is calculated for Hamiltonian in reduced-symmetry subspace
 
 ######## RUNNING CODE
 mol_name = ARGS[1]
 
 import Pkg
 
-Pkg.activate("./") # uncomment for using local QuantumMAMBO installation
-using QuantumMAMBO: DATAFOLDER, SAVELOAD_HAM, RUN_L1, symmetry_treatment, INTERACTION, bliss_optimizer, PAULI_L1, quadratic_bliss, Ne_block_diagonalizer, matrix_symmetry_block, to_matrix, bliss_linprog, quadratic_bliss_optimizer
+Pkg.activate("./")
+Pkg.instantiate()
+Pkg.resolve()
+
+using QuantumMAMBO: DATAFOLDER, SAVELOAD_HAM, RUN_L1, symmetry_treatment, INTERACTION, bliss_optimizer, quadratic_bliss, bliss_linprog, quadratic_bliss_optimizer,F_OP_converter,F_OP_compress,PAULI_L1, spectral_range,check,PySCF_type,closed_shell, ROHF, Charge, Spin
 
 
 ###### SAVELOAD ROUTINES FOR MOLECULAR HAMILTONIAN #######
@@ -34,11 +35,11 @@ H,η = SAVELOAD_HAM(mol_name, FILENAME)
 
 ###### END: SAVELOAD ROUTINES FOR MOLECULAR HAMILTONIAN #######
 
+
+
 RUN_L1(H, η=η, DO_CSA = DO_CSA, DO_DF = DO_DF, DO_ΔE = DO_ΔE, LATEX_PRINT = TABLE_PRINT, 
 	DO_FC = DO_FC, SYM_RED=DO_TROTTER, DO_AC = DO_AC, DO_OO = DO_OO, DO_THC = DO_THC, 
-	DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, 
-	COUNT = COUNT, verbose=verbose, name=FILENAME*".h5", FOCK_BOUND = FOCK_BOUND)
-
+	DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, COUNT = COUNT, verbose=verbose, name=FILENAME*".h5",compress=!PySCF_type[1],spin_symmetry=closed_shell[1],rohf=ROHF[1])
 
 if SYM_SHIFT
 	println("\n\nStarting symmetry-shift routine...")
@@ -46,8 +47,7 @@ if SYM_SHIFT
 	println("Finished obtaining symmetry shifts, running routines for shifted Hamiltonian...")
 	RUN_L1(H_SYM, η=η, DO_CSA = DO_CSA, DO_DF = DO_DF, DO_ΔE = DO_ΔE, LATEX_PRINT = TABLE_PRINT, 
 		DO_FC = DO_FC, SYM_RED=DO_TROTTER, DO_AC = DO_AC, DO_OO = DO_OO, DO_THC = DO_THC, 
-		DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, 
-		COUNT = COUNT, verbose=verbose, name=FILENAME*"_SYM.h5", FOCK_BOUND = FOCK_BOUND)
+		DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, COUNT = COUNT, verbose=verbose, name=FILENAME*"_SYM.h5")
 end
 
 if INT
@@ -56,21 +56,33 @@ if INT
 	println("Finished obtaining interaction picture Hamiltonian, starting post-processing...")
 	RUN_L1(H_INT, η=η, DO_CSA = DO_CSA, DO_DF = DO_DF, DO_ΔE = DO_ΔE, LATEX_PRINT = TABLE_PRINT, 
 		DO_FC = DO_FC, SYM_RED=DO_TROTTER, DO_AC = DO_AC, DO_OO = DO_OO, DO_THC = DO_THC, 
-		DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, 
-		COUNT = COUNT, verbose=verbose, name=FILENAME*"_INT.h5", FOCK_BOUND = FOCK_BOUND)
+		DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, COUNT = COUNT, verbose=verbose, name=FILENAME*"_INT.h5")
 end
 
 if BLISS
 	println("\n\n Starting block-invariant symmetry shift (BLISS) routine...")
 	println("BLISS optimization...")
-	H_bliss,_ = bliss_linprog(H, η)
-	
+	#H_bliss = quadratic_bliss_optimizer(H, η, verbose=verbose, SAVENAME=FILENAME*"_BLISS.h5")
+	#H_bliss = quadratic_bliss(H, η)
+	#@show spectral_range(H,false,η)
+	#check(H)
+	@show PySCF_type
+	if PySCF_type[1] == false
+		H_cmp=F_OP_compress(H)
+		H_bliss,_=bliss_linprog(H_cmp, η,SAVENAME=FILENAME*"_BLISS.h5")
+		@show H_bliss
+		#H_bliss=F_OP_converter(H_bliss)
+	else
+		H_bliss,_=bliss_linprog(H, η,SAVENAME=FILENAME*"_BLISS.h5")
+	end
+	#@show spectral_range(H_bliss,false,η)
+	#check(H_bliss)
+	#@show spectral_range(H_bliss,true,η)
+	#H_bliss=F_OP_converter(H_bliss)
 	println("Running 1-norm routines...")
-	@time H_bliss = bliss_optimizer(H, η, verbose=verbose, SAVENAME=FILENAME*"_BLISS.h5")
 	RUN_L1(H_bliss, η=η, DO_CSA = DO_CSA, DO_DF = DO_DF, DO_ΔE = DO_ΔE, LATEX_PRINT = TABLE_PRINT, 
 		DO_FC = DO_FC, SYM_RED=DO_TROTTER, DO_AC = DO_AC, DO_OO = DO_OO, DO_THC = DO_THC, 
-		DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, 
-		COUNT = COUNT, verbose=verbose, name=FILENAME*"_BLISS.h5", FOCK_BOUND = FOCK_BOUND)
+		DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, COUNT = COUNT, verbose=verbose, name=FILENAME*"_BLISS.h5",bliss=true,spin_symmetry=closed_shell[1],rohf=ROHF[1])
 end
 
 if BLISS && INT
@@ -79,19 +91,5 @@ if BLISS && INT
 	@time H_before = INTERACTION(H_bliss, SAVENAME=FILENAME*"_BLISS_INT.h5")
 	RUN_L1(H_before, η=η, DO_CSA = DO_CSA, DO_DF = DO_DF, DO_ΔE = DO_ΔE, LATEX_PRINT = TABLE_PRINT, 
 		DO_FC = DO_FC, SYM_RED=DO_TROTTER, DO_AC = DO_AC, DO_OO = DO_OO, DO_THC = DO_THC, 
-		DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, 
-		COUNT = COUNT, verbose=verbose, name=FILENAME*"_BLISS_INT.h5", FOCK_BOUND = FOCK_BOUND)
-end
-
-if SYM_SUBSPACE
-	using LinearAlgebra
-	println("\n\n Starting symmetry-subspace routine")
-	println("Building subspace-projector")
-	Uη = Ne_block_diagonalizer(H.N, η, spin_orb=H.spin_orb)
-	println("Building reduced H matrix...")
-	@time Hη = matrix_symmetry_block(to_matrix(H), Uη)
-	println("Diagonalizing symmetry-projected Hamiltonian")
-	@time E,_ = eigen(Hη)
-	λη_min = (maximum(real.(E))-minimum(real.(E)))/2
-	@show λη_min
+		DO_SQRT = DO_SQRT, DO_TROTTER=DO_TROTTER, DO_MHC = DO_MHC, DO_MTD_CP4 = DO_MTD_CP4, COUNT = COUNT, verbose=verbose, name=FILENAME*"_BLISS_INT.h5")
 end
