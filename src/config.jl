33b4d1fdb5cfa0e35b29886c69ba412513045664
Ignacio <ignatzloa@gmail.com>
2025-03-18 11:14:56 -0400

    Relaxed tolerances to more realistic values

--- src/config.jl
+++ src/config.jl
@@ -16,13 +16,13 @@ const OO_GIVENS = true #whether orbital-rotation optimization generates unitarie
 const OO_reps = 10 #how many parallel repetitions are done for orbital optimization routine
 
 #Tolerances and constants
-const ϵ = 1e-6 #decomposition fermionic 2-norm tolerance
+const ϵ = 1e-5 #decomposition fermionic 2-norm tolerance
 const ϵ_Givens = 1e-12 #tolerance for Givens decomposition using maximal torus theorem L2-norm
 const SVD_tiny = 1e-12 #tolerance for symmetry-enforcement of SVD fragments
 const SVD_tol = 1e-8 #cut-off for SVD coefficients
-const PAULI_TOL = 1e-8 #cut-off Pauli terms when building Q_OP qubit operators with magnitude smaller than this tolerance
+const PAULI_TOL = 1e-6 #cut-off Pauli terms when building Q_OP qubit operators with magnitude smaller than this tolerance
 const MAT_DROP_TOL = 1e-8 #tolerance for cutting off elements in sparse matrix when converting operator to matrix
-const LCU_tol = 1e-6 #cut-off for counting unitaries with magnitude smaller than this tolerance
+const LCU_tol = 1e-5 #cut-off for counting unitaries with magnitude smaller than this tolerance
 
 #Additional definitions
 const CONFIG_LOADED = true #flag that tracks config already being loaded, useful for redefining constants and being able to load build.jl
